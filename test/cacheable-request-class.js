410a4fe97a29b207eacb68a9c9353c98db650fcc
Jason Baik <baikjason@gmail.com>
2022-06-11 10:58:50 -0700

    update and fix ava related errors in tests

--- test/cacheable-request-class.js
+++ test/cacheable-request-class.js
@@ -1,20 +1,21 @@
-import { request } from "http";
-import * as test from "ava";
-import Keyv from "keyv";
-import CacheableRequest from "this";
+import { request } from 'node:http';
+import test from 'ava';
+import Keyv from 'keyv';
+import CacheableRequest from '../src/index.js';
+
 test('CacheableRequest is a function', t => {
-    t.is(typeof CacheableRequest, 'function');
+	t.is(typeof CacheableRequest, 'function');
 });
 test('CacheableRequest cannot be invoked without \'new\'', t => {
-    t.throws(() => CacheableRequest(request)); // eslint-disable-line new-cap
-    t.notThrows(() => new CacheableRequest(request));
+	t.throws(() => CacheableRequest(request)); // eslint-disable-line new-cap
+	t.notThrows(() => new CacheableRequest(request));
 });
 test('CacheableRequest throws TypeError if request fn isn\'t passed in', t => {
-    const error = t.throws(() => {
-        new CacheableRequest(); // eslint-disable-line no-new
-    }, undefined);
-    t.is(error.message, 'Parameter `request` must be a function');
+	const error = t.throws(() => {
+		new CacheableRequest(); // eslint-disable-line no-new
+	}, undefined);
+	t.is(error.message, 'Parameter `request` must be a function');
 });
 test('CacheableRequest accepts Keyv instance', t => {
-    t.notThrows(() => new CacheableRequest(request, new Keyv()));
+	t.notThrows(() => new CacheableRequest(request, new Keyv()));
 });
